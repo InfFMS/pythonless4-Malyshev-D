b783fa4abbb04df526eee25d1a50007f661e002b
danmalyshev2020@gmail.com <Daniil>
2024-10-21 10:09:21 +0700

    21.10

--- task4.py
+++ task4.py
@@ -1,4 +1,11 @@
 # Напишите функцию, которая находит
 # наибольший общий делитель двух натуральных чисел
 # На входе два числа, на выходе их НОД.
-
+def nod(a, b):
+    d = [1]
+    for i in range(min(a, b), 1, -1):
+        if max(a,b) % i == 0 and min(a, b) % i == 0: d.append(i)
+    print('НОД:', max(d))
+a = int(input())
+b = int(input())
+nod(a, b)
